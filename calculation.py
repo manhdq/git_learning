816f24b57aeb4f7aefcb1666d204964b31b4ffd7
manhdq <doquangmanhhb01@gmail.com>
2022-11-18 01:42:53 +0000

    Initial commit. feat(calculation) creation and add rectangle perimeter

---
+++ calculation.py
@@ -0,0 +1,13 @@
+import math
+
+def cal_rectangle_perimeter(a, b):
+    return 2*(a + b)
+
+if __name__ == '__main__':
+    f = int(input("choose function: \n\
+        0. cal_rectangle_perimeter \n\
+        Please enter an integer:"))
+
+    else:
+        result = "Wrong input"
+    print(f"\nResult: {result}")
